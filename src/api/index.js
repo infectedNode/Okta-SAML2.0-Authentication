e1dca382c07d6301997058dc84bd52dbf2e09203
Shivam Shivam <shivam.sharma1@thoughtworks.com>
2022-05-08 23:42:15 +0530

    [Shivam] | Learn. express-session

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const session = require('express-session');
+
 const router = express.Router();
 
+router.use(session({
+  secret: "never-stop-learning",
+  name: "user-session",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 1000 * 30},
+}));
+
 router.get('/', (req, res) => {
-  res.send("Hello from API server 😵")
+  
+  // Testing Sesisons
+  if(req.session.variableName) {
+    req.session.variableName++;
+  } else {
+    req.session.variableName = 1;
+  }
+  console.log(req.session);
+  console.log(req.sessionID);
+
+  res.send("Hello from API server 😵");
 });
 
 module.exports = router;
